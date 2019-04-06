b70286b8279b96a8cde3bf6082c1e2a0229ef5f4
Young Yu <yuyang_733@163.com>
2019-04-06 22:34:00 +0800

    修改版本号

--- src/main/java/org/apache/hadoop/fs/CosNativeFileSystemConfigKeys.java
+++ src/main/java/org/apache/hadoop/fs/CosNativeFileSystemConfigKeys.java
@@ -25,7 +25,7 @@ import org.apache.hadoop.classification.InterfaceStability;
 @InterfaceStability.Unstable
 public class CosNativeFileSystemConfigKeys extends CommonConfigurationKeys {
     public static final String USER_AGENT = "fs.cosn.user.agent";
-    public static final String DEFAULT_USER_AGENT = "cos-hadoop-plugin-v5.5.1";
+    public static final String DEFAULT_USER_AGENT = "cos-hadoop-plugin-v5.5.2";
 
     public static final String COS_CREDENTIALS_PROVIDER = "fs.cosn.credentials.provider";
     public static final String COS_APPID_KEY = "fs.cosn.userinfo.appid";
