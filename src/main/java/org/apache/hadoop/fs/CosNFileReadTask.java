59543bfc3c2d7ff726a8fce837fb3f3d2eddd8bd
alantong(佟明达) <alantong@tencent.com>
2021-06-18 12:28:48 +0800

    remove the retrypolicy outside the get object

--- src/main/java/org/apache/hadoop/fs/CosNFileReadTask.java
+++ src/main/java/org/apache/hadoop/fs/CosNFileReadTask.java
@@ -2,16 +2,11 @@ package org.apache.hadoop.fs;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.io.IOUtils;
-import org.apache.hadoop.io.retry.RetryPolicies;
-import org.apache.hadoop.io.retry.RetryPolicy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 public class CosNFileReadTask implements Runnable {
     static final Logger LOG = LoggerFactory.getLogger(CosNFileReadTask.class);
@@ -19,8 +14,6 @@ public class CosNFileReadTask implements Runnable {
     private final String key;
     private final NativeFileSystemStore store;
     private final CosFsInputStream.ReadBuffer readBuffer;
-
-    private RetryPolicy retryPolicy = null;
 
     /**
      * cos file read task
@@ -35,76 +28,32 @@ public class CosNFileReadTask implements Runnable {
         this.key = key;
         this.store = store;
         this.readBuffer = readBuffer;
-
-        RetryPolicy defaultPolicy =
-                RetryPolicies.retryUpToMaximumCountWithFixedSleep(
-                        conf.getInt(
-                                CosNConfigKeys.COSN_MAX_RETRIES_KEY,
-                                CosNConfigKeys.DEFAULT_MAX_RETRIES),
-                        conf.getLong(
-                                CosNConfigKeys.COSN_RETRY_INTERVAL_KEY,
-                                CosNConfigKeys.DEFAULT_RETRY_INTERVAL),
-                        TimeUnit.SECONDS
-                );
-        Map<Class<? extends Exception>, RetryPolicy> retryPolicyMap =
-                new HashMap<Class<? extends Exception>, RetryPolicy>();
-        retryPolicyMap.put(IOException.class, defaultPolicy);
-        retryPolicyMap.put(IndexOutOfBoundsException.class,
-                RetryPolicies.TRY_ONCE_THEN_FAIL);
-        retryPolicyMap.put(NullPointerException.class,
-                RetryPolicies.TRY_ONCE_THEN_FAIL);
-
-        this.retryPolicy = RetryPolicies.retryByException(defaultPolicy,
-                retryPolicyMap);
     }
 
     @Override
     public void run() {
-        int retries = 0;
-        RetryPolicy.RetryAction retryAction = null;
         try {
             this.readBuffer.lock();
-            do {
-                try {
-                    InputStream inputStream = this.store.retrieveBlock(
-                            this.key, this.readBuffer.getStart(),
-                            this.readBuffer.getEnd());
-                    IOUtils.readFully(
-                            inputStream, this.readBuffer.getBuffer(), 0,
-                            readBuffer.getBuffer().length);
-                    int readEof = inputStream.read();
-                    if (readEof != -1) {
-                        LOG.error("Expect to read the eof, but the return is not -1. key: {}.", this.key);
-                    }
-                    inputStream.close();
-                    this.readBuffer.setStatus(CosFsInputStream.ReadBuffer.SUCCESS);
-                    break;
-                } catch (IOException e) {
-                    this.readBuffer.setStatus(CosFsInputStream.ReadBuffer.ERROR);
-                    LOG.warn("Exception occurs when retrieve the block range " +
-                            "start: "
-                            + String.valueOf(this.readBuffer.getStart()) + " " +
-                            "end: " + this.readBuffer.getEnd(), e);
-                    try {
-                        retryAction = this.retryPolicy.shouldRetry(e,
-                                retries++, 0, true);
-                        if (null != retryAction && retryAction.action == RetryPolicy.RetryAction.RetryDecision.RETRY) {
-                            Thread.sleep(retryAction.delayMillis);
-                        }
-                    } catch (Exception e1) {
-                        String errMsg = String.format("Exception occurs when " +
-                                        "retry[%s] "
-                                        + "to retrieve the block range start:" +
-                                        " %d, end:%d",
-                                this.retryPolicy.toString(),
-                                this.readBuffer.getStart(),
-                                this.readBuffer.getEnd());
-                        LOG.error(errMsg, e1);
-                        break;
-                    }
+            try {
+                InputStream inputStream = this.store.retrieveBlock(
+                        this.key, this.readBuffer.getStart(),
+                        this.readBuffer.getEnd());
+                IOUtils.readFully(
+                        inputStream, this.readBuffer.getBuffer(), 0,
+                        readBuffer.getBuffer().length);
+                int readEof = inputStream.read();
+                if (readEof != -1) {
+                    LOG.error("Expect to read the eof, but the return is not -1. key: {}.", this.key);
                 }
-            } while (null != retryAction && retryAction.action == RetryPolicy.RetryAction.RetryDecision.RETRY);
-
+                inputStream.close();
+                this.readBuffer.setStatus(CosFsInputStream.ReadBuffer.SUCCESS);
+            } catch (IOException e) {
+                this.readBuffer.setStatus(CosFsInputStream.ReadBuffer.ERROR);
+                LOG.error("Exception occurs when retrieve the block range " +
+                        "start: "
+                        + String.valueOf(this.readBuffer.getStart()) + " " +
+                        "end: " + this.readBuffer.getEnd(), e);
+            }
             this.readBuffer.signalAll();
         } finally {
             this.readBuffer.unLock();
